4ce73e70650256f83a06b0b16bc0e7340de8c9c2
Ewelina Jeschke <ewelina.jeschke@gmail.com>
2021-10-07 21:57:45 +0200

    atributes added to class: Doctor and Receptionist

--- class Doctor.py
+++ class Doctor.py
@@ -1,4 +1,47 @@
 class Doctor:
+    number = 0
+    def __init__(self, name, surname, login, password, PESEL, hire_date, position, function, sex, account_status,
+                     street, city, postal_code, phone_number, email, specialty, location):
+        self.id = self.number
+        self.name = name
+        self.surname = surname
+        self.login = login
+        self.password = password
+        self.PESEL = PESEL
+        self.hire_date = hire_date
+        self.position = position
+        self.function = function
+        self.sex = sex
+        self.account_status = account_status
+        self.street = street
+        self.city = city
+        self.postal_code = postal_code
+        self.phone_number = phone_number
+        self.email = email
+        self.specialty = specialty
+        self.location = location
+        Doctor.number += 1
 
-    def __init__(self):
-        pass
+    def add_availability_to_calendar(self):
+        print('Wizyta została dodana do kalendarza.')
+
+    def edit_availability_in_calendar(self):
+        print('Wizyta została edytowana w kolendarzu.')
+
+    def show_availability_in_calendar(self):
+        print('Dostępność została wyświetlona.')
+
+    def remove_availability_from_calendar(self):
+        print('Wizyta została usunięta z kalendarza.')
+
+    def register_visit(self):
+        print('Wizyta została zarejestrowana.')
+
+    def add_comments_to_visit(self):
+        print('Komentarz został dodany do wizyty.')
+
+    def write_prescription(self):
+        print('Recepta została wystawiona.')
+
+    def issue_a_sick_leave(self):
+        print('Zwolnienie lekarskie zostało wystawione.')
--- class Receptionist.py
+++ class Receptionist.py
@@ -1,4 +1,47 @@
 class Receptionist:
+    number = 0
+    def __init__(self, name, surname, login, password, PESEL, hire_date, position, function, sex, account_status, street, city, postal_code, phone_number, email):
+        self.id = self.number
+        self.name = name
+        self.surname = surname
+        self.login = login
+        self.password = password
+        self.PESEL = PESEL
+        self.hire_date = hire_date
+        self.position = position
+        self.function = function
+        self.sex = sex
+        self.account_status = account_status
+        self.street = street
+        self.city = city
+        self.postal_code = postal_code
+        self.phone_number = phone_number
+        self.email = email
+        Receptionist.number += 1
 
-    def __init__(self):
-        pass
+    def add_new_patient(self):
+        print('Nowy pacjent został dodany.')
+
+    def edit_patient(self):
+        print('Dane pacjenta zostały zmienione.')
+
+    def show_patient(self):
+        print('Dane pacjenta zostały wyświetlone.')
+
+    def remove_patient(self):
+        print('Pacjent został usunięty.')
+
+    def schedule_doctor_appointment(self):
+        print('Pacjent został zapisany na wizytę lekarską.')
+
+    def edit_doctor_appointment(self):
+        print('Wizyta lekarska została zmieniona.')
+
+    def show_doctor_appointment(self):
+        print('Wizyta lekarska została wyświetlona.')
+
+    def remove_doctor_appointment(self):
+        print('Wizyta lekarska została usunięta.')
+
+receptionist1 = Receptionist
+print(receptionist1)
