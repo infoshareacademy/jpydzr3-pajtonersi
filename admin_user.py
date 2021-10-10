dcd81ce9cbb18d489a2d643c218af7587b7dc748
Jarek Majka <vessekheal@gmail.com>
2021-10-10 11:24:23 +0200

    Admin class updated

--- admin_user.py
+++ admin_user.py
@@ -3,8 +3,8 @@ from master_user import User
 
 class Admin(User):
     def __init__(self, first_name: str, last_name: str, pesel: int, gender: str, phone_no: str, city: str,
-                 address: str):
-        super().__init__(first_name, last_name, pesel, gender, phone_no, city, address)
+                 address: str, postal_code: str, email: str):
+        super().__init__(first_name, last_name, pesel, gender, phone_no, city, address, postal_code, email)
 
     def display_patient_list(self):
         # TODO implement patient list display
