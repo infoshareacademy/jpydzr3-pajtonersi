b7317711e3fcc1ac278d45208a2e76fa2854700b
Jarek Majka <vessekheal@gmail.com>
2021-10-15 12:41:05 +0200

    User authentication service added

--- users/user_services.py
+++ users/user_services.py
@@ -1,2 +1,10 @@
+import login_screen
+
+
 class UserServices:
-    pass
+    def __init__(self):
+        pass
+
+    def user_authentication(self) -> object:
+        """Authenticates user and return instance of user object"""
+        login_screen.log_in()
