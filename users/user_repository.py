61403518ba37ed71bcc6d6ab795a2b08623881af
Jarek Majka <vessekheal@gmail.com>
2021-10-19 22:59:01 +0200

    add_user method skeleton created

--- users/user_repository.py
+++ users/user_repository.py
@@ -1,4 +1,12 @@
+from enum import Enum
+
 from user_classes.admin_user import Admin
+
+
+class UserChoiceEnum(Enum):
+    ADD_PATIENT = 1
+    ADD_DOCTOR = 2
+    ADD_RECEPTIONIST = 3
 
 
 class UserRepository:
@@ -6,10 +14,44 @@ class UserRepository:
         self.current_user = current_user
 
     def add_user(self):
-        if isinstance(self.current_user, Admin):
-            # TODO User ENUM
+        def print_options():
             print('1. Dodaj pacjenta.')
             print('2. Dodaj lekarza.')
             print('3. Dodaj recepcjonistkę.')
-            user_choice = input('Wybierz jedną z opcji: ')
 
+        def validate_decision(decision):
+            ALLOWED_DECISIONS = [option.value for option in UserChoiceEnum]
+            if decision not in ALLOWED_DECISIONS:
+                print(f'Liczba {decision} jest niedozwolona.')
+                raise ValueError
+
+        def add_doctor():
+            # TODO Implement function
+            print('Creating a doc')
+
+        def add_receptionist():
+            # TODO Implement function
+            print('Creating a receptionist')
+
+        def add_patient():
+            # TODO Implement function
+            print('Creating a patient')
+
+        def execute_decision(decision):
+            functions = {
+                UserChoiceEnum.ADD_PATIENT.value: add_patient,
+                UserChoiceEnum.ADD_DOCTOR.value: add_doctor,
+                UserChoiceEnum.ADD_RECEPTIONIST.value: add_receptionist,
+            }
+            functions[decision]()
+
+        if isinstance(self.current_user, Admin):
+            print_options()
+            user_choice = int(input('Wybierz jedną z opcji: '))
+            validate_decision(user_choice)
+            execute_decision(user_choice)
+
+
+# TODO Remove below tests
+admin_user = Admin('a', 'b', 1, 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l')
+UserRepository(admin_user).add_user()
